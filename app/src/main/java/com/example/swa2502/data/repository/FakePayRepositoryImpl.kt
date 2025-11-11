3987effc33dc620565e8ee910d98eb13ed688e7a
casperjr <hyunjw0515@gmail.com>
2025-11-11 15:36:59 +0900

    [CHORE]: 가상 시뮬레이션으로 구현할 결제에 필요한 로직을 구현할 클래스, 인터페이스 틀 작성

---
+++ app/src/main/java/com/example/swa2502/data/repository/FakePayRepositoryImpl.kt
@@ -0,0 +1,8 @@
+package com.example.swa2502.data.repository
+
+import com.example.swa2502.domain.repository.PayRepository
+
+class FakePayRepositoryImpl(): PayRepository {
+    // 가상의 결제를 수행할 로직 작성
+
+}
