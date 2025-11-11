cfd5b1f25a6c27c4748459479e65d97f215618f5
casperjr <hyunjw0515@gmail.com>
2025-11-11 15:36:24 +0900

    [CHORE]: 대기열 조회에 필요한 로직을 구현할 클래스, 인터페이스 틀 작성

---
+++ app/src/main/java/com/example/swa2502/data/datasource/QueueDataSource.kt
@@ -0,0 +1,9 @@
+package com.example.swa2502.data.datasource
+
+import com.example.swa2502.data.api.QueueApi
+
+class QueueDataSource(
+    private val api: QueueApi
+) {
+
+}
---
+++ app/src/main/java/com/example/swa2502/domain/repository/QueueRepository.kt
@@ -0,0 +1,5 @@
+package com.example.swa2502.domain.repository
+
+interface QueueRepository {
+    // 대기열 조회, 혼잡도 조회와 같은 작업을 수행
+}
