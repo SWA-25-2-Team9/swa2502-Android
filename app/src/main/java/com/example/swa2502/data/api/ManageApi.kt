67567ae08ed658ed813f12fafc1f6791eb07850a
casperjr <hyunjw0515@gmail.com>
2025-11-11 15:36:13 +0900

    [CHORE]: 주문 관리에 필요한 로직을 구현할 클래스, 인터페이스 틀 작성

---
+++ app/src/main/java/com/example/swa2502/data/api/ManageApi.kt
@@ -0,0 +1,5 @@
+package com.example.swa2502.data.api
+
+interface ManageApi {
+    // 실제 API 요청을 보내는 함수를 작성
+}
---
+++ app/src/main/java/com/example/swa2502/domain/repository/ManageRepository.kt
@@ -0,0 +1,4 @@
+package com.example.swa2502.domain.repository
+
+interface ManageRepository {
+}
