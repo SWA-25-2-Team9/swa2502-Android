df960ab84d9c1cc60a59e9f8122b9caf1b31b640
casperjr <hyunjw0515@gmail.com>
2025-11-13 21:23:53 +0900

    [CHORE]: 사용할 Route들 정의

--- app/src/main/java/com/example/swa2502/presentation/navigation/Route.kt
+++ app/src/main/java/com/example/swa2502/presentation/navigation/Route.kt
@@ -2,12 +2,26 @@ package com.example.swa2502.presentation.navigation
 
 sealed class Route(val route: String) {
     // 로그인, 회원가입
+    object Login: Route(route = "login")
+    object SighUp: Route(route = "signUp")
 
     // 관리자 메인
+    object ManageQueue: Route(route = "manageRoute")
 
     // 사용자 메인
+    object ShopOverview: Route(route = "shopOverview")
 
     // 주문 관련
+    object OrderMenu: Route(route = "orderMenu")
+    object MenuOption: Route(route = "menuOption")
+    object ShoppingCart: Route("shoppingCart")
+    object Pay: Route(route = "pay")
+    object PayResult: Route(route = "payResult")
 
     // 혼잡도 조회
+    object RestaurantQueue: Route(route = "restaurantQueue")
+    object RestaurantQueueDetails: Route(route = "restaurantQueueDetails")
+
+    // 마이페이지
+    object MyPage: Route(route = "myPage")
 }
