c20684a9f7600ccd653abaee41a74adb8d7ad904
casperjr <hyunjw0515@gmail.com>
2025-11-13 21:10:21 +0900

    [CHORE]: 네비게이션에 사용할 Route 클래스 생성

---
+++ app/src/main/java/com/example/swa2502/presentation/navigation/Route.kt
@@ -0,0 +1,13 @@
+package com.example.swa2502.presentation.navigation
+
+sealed class Route(val route: String) {
+    // 로그인, 회원가입
+
+    // 관리자 메인
+
+    // 사용자 메인
+
+    // 주문 관련
+
+    // 혼잡도 조회
+}
