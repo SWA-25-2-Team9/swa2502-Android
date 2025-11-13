3b36dbedd9b37ad74b530c3585b86cdd51ebc1a9
casperjr <hyunjw0515@gmail.com>
2025-11-13 13:34:57 +0900

    [CHORE]: 네트워크 의존성 모듈 작성

---
+++ app/src/main/java/com/example/swa2502/core/di/NetworkModule.kt
@@ -0,0 +1,65 @@
+package com.example.swa2502.core.di
+
+import com.example.swa2502.BuildConfig
+import com.example.swa2502.core.network.AuthInterceptor
+import com.example.swa2502.core.network.TokenAuthenticator
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesJson(): Json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+        coerceInputValues = true
+    }
+
+    @Provides
+    @Singleton
+    fun providesConverterFactory(): Converter.Factory =
+        Json.asConverterFactory("application/json".toMediaType())
+
+    @Provides
+    @Singleton
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    @Singleton
+    fun providesDefaultOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        authInterceptor: AuthInterceptor,
+        tokenAuthenticator: TokenAuthenticator
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            addInterceptor(loggingInterceptor)
+            addInterceptor(authInterceptor)
+            authenticator(tokenAuthenticator)
+        }.build()
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(
+        client: OkHttpClient,
+        converterFactory: Converter.Factory
+    ): Retrofit = Retrofit.Builder()
+        .baseUrl(BuildConfig.BASE_URL)
+        .client(client)
+        .addConverterFactory(converterFactory)
+        .build()
+}
