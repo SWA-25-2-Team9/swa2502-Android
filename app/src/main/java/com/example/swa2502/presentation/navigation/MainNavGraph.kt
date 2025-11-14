c384ecc56301151282026c74898bb82adf029fc4
casperjr <hyunjw0515@gmail.com>
2025-11-14 12:24:35 +0900

    [CHORE]: 네비게이션에 screen 추가

--- app/src/main/java/com/example/swa2502/presentation/navigation/MainNavGraph.kt
+++ app/src/main/java/com/example/swa2502/presentation/navigation/MainNavGraph.kt
@@ -4,6 +4,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.swa2502.presentation.ui.auth.LoginScreen
+import com.example.swa2502.presentation.ui.auth.SignUpScreen
+import com.example.swa2502.presentation.ui.manage.ManageQueueScreen
+import com.example.swa2502.presentation.ui.mypage.MyPageScreen
+import com.example.swa2502.presentation.ui.order.MenuOptionScreen
+import com.example.swa2502.presentation.ui.order.OrderMenuScreen
+import com.example.swa2502.presentation.ui.order.ShoppingCartScreen
+import com.example.swa2502.presentation.ui.pay.PayResultScreen
+import com.example.swa2502.presentation.ui.pay.PayScreen
+import com.example.swa2502.presentation.ui.queue.RestaurantQueueDetailScreen
+import com.example.swa2502.presentation.ui.queue.RestaurantQueueScreen
+import com.example.swa2502.presentation.ui.shop.ShopOverviewScreen
 
 @Composable
 fun MainNavGraph(
@@ -15,15 +28,55 @@ fun MainNavGraph(
         startDestination = Route.Login.route
     ){
         /* 로그인, 회원가입 */
-
+        composable(route = Route.Login.route){
+            LoginScreen()
+        }
+        composable(route = Route.SighUp.route){
+            SignUpScreen()
+        }
         /* 관리자 */
+        composable(route = Route.ManageQueue.route){
+            ManageQueueScreen()
+        }
 
         /* 사용자 */
+        composable(route = Route.ShopOverview.route){
+            ShopOverviewScreen()
+        }
 
         /* 주문 */
+        composable(route = Route.OrderMenu.route){
+            OrderMenuScreen()
+        }
+
+        composable(route = Route.MenuOption.route){
+            MenuOptionScreen()
+        }
+
+        composable(route = Route.ShoppingCart.route){
+            ShoppingCartScreen()
+        }
+
+        /* 결제 */
+        composable(route = Route.Pay.route){
+            PayScreen()
+        }
+
+        composable(route = Route.PayResult.route){
+            PayResultScreen()
+        }
 
         /* 혼잡도 */
+        composable(route = Route.RestaurantQueue.route){
+            RestaurantQueueScreen()
+        }
+        composable(route = Route.RestaurantQueueDetail.route){
+            RestaurantQueueDetailScreen()
+        }
 
         /* 마이페이지 */
+        composable(route = Route.MyPage.route){
+            MyPageScreen()
+        }
     }
 }
