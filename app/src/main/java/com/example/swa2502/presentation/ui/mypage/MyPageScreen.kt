95c34edfabe6f3dc6a8b6dc8836ff128bd05d878
casperjr <hyunjw0515@gmail.com>
2025-11-14 12:24:07 +0900

    [CHORE]: mypage screen, viewmodel 생성

---
+++ app/src/main/java/com/example/swa2502/presentation/ui/mypage/MyPageScreen.kt
@@ -0,0 +1,21 @@
+package com.example.swa2502.presentation.ui.mypage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun MyPageScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+fun MyPageScreenContent(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+private fun MyPageScreenPreview() {
+
+}
---
+++ app/src/main/java/com/example/swa2502/presentation/viewmodel/mypage/MyPageViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.swa2502.presentation.viewmodel.mypage
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class MyPageViewModel @Inject constructor(
+
+) : ViewModel() {
+
+}
