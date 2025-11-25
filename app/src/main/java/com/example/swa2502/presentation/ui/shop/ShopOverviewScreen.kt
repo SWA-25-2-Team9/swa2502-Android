ce183b15faefb5f7480a660b9e7c2372da168799
casperjr <hyunjw0515@gmail.com>
2025-11-25 21:43:21 +0900

    [UI]: 가게별 대기열 ui 구현

--- app/src/main/java/com/example/swa2502/presentation/ui/shop/ShopOverviewScreen.kt
+++ app/src/main/java/com/example/swa2502/presentation/ui/shop/ShopOverviewScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -37,17 +39,23 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.swa2502.R
+import com.example.swa2502.domain.model.ShopOverviewInfo
+import com.example.swa2502.presentation.ui.shop.component.ShopOverviewItem
 import com.example.swa2502.presentation.viewmodel.shop.ShopOverviewUiState
 import com.example.swa2502.presentation.viewmodel.shop.ShopOverviewViewModel
 
 @Composable
-fun ShopOverviewScreen(modifier: Modifier = Modifier) {
+fun ShopOverviewScreen(
+    modifier: Modifier = Modifier,
+    onNavigateToOrder: () -> Unit,
+) {
     val viewModel: ShopOverviewViewModel = hiltViewModel()
     val uiState = viewModel.uiState.collectAsStateWithLifecycle()
 
     ShopOverviewScreenContent(
         modifier = Modifier,
         uiState = uiState.value,
+        onNavigateToOrder = onNavigateToOrder
     )
 }
 
@@ -56,12 +64,14 @@ fun ShopOverviewScreen(modifier: Modifier = Modifier) {
 fun ShopOverviewScreenContent(
     modifier: Modifier = Modifier,
     uiState: ShopOverviewUiState,
+    onNavigateToOrder: () -> Unit,
 ) {
     var dropdownExpanded by remember { mutableStateOf(false) }
     val dropdownList = uiState.restaurantList
     var selectedDropdownMenu by rememberSaveable { 
         mutableStateOf(dropdownList.firstOrNull() ?: "")
     }
+    val scrollState = rememberScrollState()
 
     Column(
         modifier = Modifier.fillMaxSize()
@@ -146,10 +156,21 @@ fun ShopOverviewScreenContent(
         
         Column(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
                 .padding(horizontal = 20.dp)
+                .verticalScroll(state = scrollState)
         ) {
-
+            uiState.shopInfoList.forEach { shopInfo ->
+                ShopOverviewItem(
+                    modifier = Modifier,
+                    shopId = shopInfo.shopId,
+                    shopState = shopInfo.shopState,
+                    shopName = shopInfo.shopName,
+                    queueSize = shopInfo.queueSize,
+                    onNavigateToOrder = onNavigateToOrder
+                )
+                Spacer(modifier = Modifier.size(12.dp))
+            }
         }
     }
 }
@@ -159,6 +180,28 @@ fun ShopOverviewScreenContent(
 private fun ShopOverviewScreenContentPreview() {
     ShopOverviewScreenContent(
         modifier = Modifier,
-        uiState = ShopOverviewUiState(),
+        uiState = ShopOverviewUiState(
+            shopInfoList = listOf(
+                ShopOverviewInfo(
+                    shopId = "111",
+                    shopState = "여유",
+                    shopName = "바비든든",
+                    queueSize = 3,
+                ),
+                ShopOverviewInfo(
+                    shopId = "222",
+                    shopState = "조금 혼잡",
+                    shopName = "폭풍분식",
+                    queueSize = 50,
+                ),
+                ShopOverviewInfo(
+                    shopId = "333",
+                    shopState = "혼잡",
+                    shopName = "경성돈카츠",
+                    queueSize = 100,
+                ),
+            )
+        ),
+        onNavigateToOrder = {}
     )
 }
