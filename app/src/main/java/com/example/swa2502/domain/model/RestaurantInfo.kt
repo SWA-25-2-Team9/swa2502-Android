95c00d47982816a42503c81f2be016188cf63a5a
casperjr <hyunjw0515@gmail.com>
2025-11-27 01:00:41 +0900

    [CHORE]: 식당에 사용할 domain model 생성

---
+++ app/src/main/java/com/example/swa2502/domain/model/RestaurantInfo.kt
@@ -0,0 +1,8 @@
+package com.example.swa2502.domain.model
+
+data class RestaurantInfo(
+    val restaurantId: String,
+    val restaurantName: String,
+    val restaurantState: String,
+    val waitingTime: Int,
+)
