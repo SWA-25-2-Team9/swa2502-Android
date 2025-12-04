4958241124db3e89e738d9eed9d76775e29c11a6
casper-jr <hyunjw0515@gmail.com>
2025-12-04 12:56:02 +0900

    endpoint 수정

--- app/src/main/java/com/example/swa2502/data/api/AuthApi.kt
+++ app/src/main/java/com/example/swa2502/data/api/AuthApi.kt
@@ -7,7 +7,7 @@ import retrofit2.http.POST
 
 interface AuthApi {
     // 실제 API 요청을 보내는 함수를 작성
-    @POST("members/log-in")
+    @POST("api/v1/members/log-in")
     suspend fun login(
         @Body request: LoginRequest
     ): LoginResponse
